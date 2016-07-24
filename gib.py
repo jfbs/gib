53acbfda1af371a36a3a7455ba62179f04e00aef
jfbs <de@ez.nz>
2016-07-24 19:25:57 -0400

    spacing fixed

--- gib.py
+++ gib.py
@@ -11,15 +11,15 @@ maxlines = len(lines)
 sentence_seed = randint(3, 7)
 
 def paragraph(num):
-    sentence = ""
+    paragph = ""
     for y in range(0, num):
         words_seed = randint(3,18)
         words = ""
         for x in range(0, words_seed):
             random_line = randint(0, maxlines)
             words += lines[random_line].rstrip() + " "
-        sentence +=  words.capitalize().rstrip() + "."
-    return sentence
+        paragph +=  words.capitalize().rstrip() + ". "
+    return paragph
 
 print(paragraph(sentence_seed))
 
