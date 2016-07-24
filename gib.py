bf81da1e8695ce46937f1fecf2774023cb464f75
jfbs <de@ez.nz>
2016-07-24 17:04:30 -0400

    use sentence method

--- gib.py
+++ gib.py
@@ -5,19 +5,22 @@ print random length sentences.
 """
 
 from random import randint
-
 fo = open('words.txt', "rb")
 lines = fo.readlines()
 maxlines = len(lines)
 
-words_seed = randint(3,18) # seed words per sentence
+ # seed words per sentence
 sentence_seed = randint(3, 6)
+def sentence():
+    words_seed = randint(3,18)
+    str = ""
+    for x in range(0, words_seed):
+        random_line = randint(0, maxlines)
+        str += lines[random_line].rstrip() + " "
 
-str = ""
-for x in range(0, words_seed):
-    random_line = randint(0, maxlines)
-    str += lines[random_line].rstrip() + " "
+    return str.capitalize().rstrip() + "."
 
-print str.capitalize().rstrip() + "."
+# print str.capitalize().rstrip() + "."
+print(sentence())
 
 fo.close()
